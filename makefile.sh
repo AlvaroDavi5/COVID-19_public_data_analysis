d92a2feb24ef7f2c5b44c3b3209d0d7f74294844
Alvaro <alvaro-davi1@hotmail.com>
2020-12-03 00:20:17 -0300

    PARA ENVIAR

--- makefile.sh
+++ makefile.sh
@@ -1,7 +1,7 @@
  #!/bin/bash
 
 echo -e "\033[1;33m Compilando programa, aguarde... \033[0m"
-gcc Programa_Final.c -o programafinal -lm
+gcc PROG2-2020_2020101874_2018104712.c -o programafinal -lm
 echo "Pronto!"
 echo ''
 echo -e "Um novo arquivo executável chamado \033[1;36m'programafinal'\033[0m foi gerado."
