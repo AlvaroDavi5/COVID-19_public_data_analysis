0f8dd216dfafa48f632398e47e4fa5f35bc2b552
Alvaro <alvaro-davi1@hotmail.com>
2020-12-02 23:50:07 -0300

    VERSAO FINAL

--- makefile.sh
+++ makefile.sh
@@ -1,7 +1,7 @@
  #!/bin/bash
 
 echo -e "\033[1;33m Compilando programa, aguarde... \033[0m"
-gcc PRINCIPAL.c -o programafinal -lm
+gcc Programa_Final.c -o programafinal -lm
 echo "Pronto!"
 echo ''
 echo -e "Um novo arquivo executável chamado \033[1;36m'programafinal'\033[0m foi gerado."
