888ec66cafa5b6e49ae7600841854b792b1e5af3
Alvaro Davi <alvaro-davi1@hotmail.com>
2021-01-29 23:25:54 -0300

    Versão Final

--- makefile.sh
+++
@@ -1,9 +0,0 @@
- #!/bin/bash
-
-echo -e "\033[1;33m Compilando programa, aguarde... \033[0m"
-gcc PROG2-2020_2020101874_2018104712.c -o programafinal -lm
-echo "Pronto!"
-echo ''
-echo -e "Um novo arquivo executável chamado \033[1;36m'programafinal'\033[0m foi gerado."
-echo ''
-
